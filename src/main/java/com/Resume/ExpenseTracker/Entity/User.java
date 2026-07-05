e17a58e6bb4d20215b4e6d373caedb16ece6760e
sunny <mohdsanidulsattar@gmail.com>
2026-07-05 22:52:16 +0530

    connected with postgres db

--- src/main/java/com/Resume/ExpenseTracker/Entity/User.java
+++ src/main/java/com/Resume/ExpenseTracker/Entity/User.java
@@ -1,16 +1,20 @@
 package com.Resume.ExpenseTracker.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+import java.util.UUID;
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    private String id;
+    @Id
+    private UUID id;
     private String userName;
     private String password;
     private String currency;
--- src/main/java/com/Resume/ExpenseTracker/Services/UserService.java
+++ src/main/java/com/Resume/ExpenseTracker/Services/UserService.java
@@ -1,6 +1,8 @@
 package com.Resume.ExpenseTracker.Services;
 
 import com.Resume.ExpenseTracker.Entity.User;
+import com.Resume.ExpenseTracker.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -9,12 +11,14 @@ import java.util.List;
 
 @Service
 public class UserService {
-    private List<User> userList = new ArrayList<>(List.of(
-            new User("001","Test1", "pass1","inr","Active"),
-            new User("002","Test2", "pass2", "usd","hold")
-    ));
+//    private List<User> userList = new ArrayList<>(List.of(
+//            new User("001","Test1", "pass1","inr","Active"),
+//            new User("002","Test2", "pass2", "usd","hold")
+//    ));
+    @Autowired
+    private UserRepository userRepository;
 
     public List<User>  getAllUser() {
-        return userList;
+        return userRepository.findAll();
     }
 }
